7f9633994a4b7b69951030b31fa2410723ab035e
MrB-g <thura.bb.2003@gmail.com>
2023-01-11 17:28:21 +0630

    Add method to read data from text file

--- main.cpp
+++ main.cpp
@@ -2,34 +2,43 @@
 #include <string>
 #include <cstdint>
 #include <vector>
+#include <fstream>
+#include <map>
 
 #include "./header/studentRecordSystem.h"
+
 #include "./resource/student.cpp"
 #include "./resource/course.cpp"
 #include "./resource/grade.cpp"
 #include "./resource/studentRecord.cpp"
+#include "./resource/textOperator.cpp"
 
 using namespace std;
+
 void initializer();
+void readAndAddData(string, string, vector<course>&, vector<student>&, vector<grade>&);
+GRADESYMBOL enumGradeSymbolConverter(string);
 
 studentRecord SR;
 
 int main(){
     initializer();
     int stdId;
-
     cout << "Type Student ID : ";
     cin >> stdId;
-
     SR.getReportCard(stdId);
-
     return 0;
 }
 
+
 void initializer(){
-    vector<course> courses = {course(1, "Algebra", 5), course(2, "Physics", 4), course(3, "English", 3), course(4, "Economics", 4)};
-    vector<student> students = {student(1, "Thura"), student(2, "BBee")};
-    vector<grade> grades = {grade(1, 1, B), grade(1, 2, A), grade(1, 3, C), grade(2, 1, A), grade(2, 2, A), grade(2, 3, B)};
+    vector<course> courses;
+    vector<student> students;
+    vector<grade> grades;
+
+    readAndAddData("./data/courses.txt", "course", courses, students, grades);
+    readAndAddData("./data/students.txt", "student", courses, students, grades);
+    readAndAddData("./data/grades.txt", "grade", courses, students, grades);
 
     for(course& cour: courses){
         SR.addCourse(cour);
@@ -41,5 +50,69 @@ void initializer(){
 
     for(grade& grad: grades){
         SR.addGrade(grad);
+    }
+}
+
+GRADESYMBOL enumGradeSymbolConverter(string studentGrade){
+    map<string, GRADESYMBOL> gradeMap;
+    gradeMap["A"] = GRADESYMBOL::A;
+    gradeMap["B"] = GRADESYMBOL::B;
+    gradeMap["C"] = GRADESYMBOL::C;
+    gradeMap["D"] = GRADESYMBOL::D;
+    gradeMap["E"] = GRADESYMBOL::E;
+    gradeMap["F"] = GRADESYMBOL::F;
+    return gradeMap[studentGrade];
+}
+
+void readAndAddData(string filePath, string type, vector<course>& cour, vector<student>& stud, vector<grade>& grad){
+    fstream readFile;
+    readFile.open(filePath, ios::in);
+    if(readFile.is_open()){
+        string output;
+        while(readFile){
+            if(type == "course"){
+                int id, courseCredit;
+                string courseName;
+                getline(readFile, output);
+                try {
+                    id = stoi(output);
+                } catch(const invalid_argument& e){}
+                getline(readFile, output);
+                courseName = output;
+                getline(readFile, output);
+                try {
+                    courseCredit = stoi(output);
+                } catch(const invalid_argument& e){}
+                getline(readFile, output);
+                cour.push_back(course(id, courseName, courseCredit));
+            } else if(type == "student"){
+                int id;
+                string studentName;
+                getline(readFile, output);
+                try { 
+                    id = stoi(output);
+                } catch(const invalid_argument& e){}
+                getline(readFile, output);
+                studentName = output;
+                getline(readFile, output);
+                stud.push_back(student(id, studentName));
+            } else if(type == "grade"){
+                int studentId, courseId;
+                GRADESYMBOL studentGrade;
+                getline(readFile, output);
+                try {
+                    studentId = stoi(output);
+                } catch(const invalid_argument& e){}
+                getline(readFile, output);
+                try {
+                    courseId = stoi(output);
+                } catch(const invalid_argument& e){}
+                getline(readFile, output);
+                studentGrade = enumGradeSymbolConverter(output);
+                getline(readFile, output);
+                grad.push_back(grade(studentId, courseId, studentGrade));
+            }
+        }
+        readFile.close();
     }
 }
